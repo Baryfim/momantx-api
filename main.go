4b7c652a7cfbd95d9d07c2d24a67b45066cbae44
Baryfim <baryfim@gmail.com>
2024-02-05 03:59:54 +0300

    Update MiddleWare

--- main.go
+++ main.go
@@ -58,7 +58,7 @@ func main() {
 	app.Use(cors.New(cors.Config{
 		AllowOrigins:     "*",
 		AllowMethods:     "GET,POST,PUT,DELETE",
-		AllowHeaders:     "Origin, Content-Type, Accept, User-Agent, Cookie",
+		AllowHeaders:     "Origin, Content-Type, Accept, User-Agent, Authorization",
 		AllowCredentials: true,
 	}))
 
